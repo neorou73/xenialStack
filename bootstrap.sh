3cb3d4eae9cecd40003b9898f17c59a741ef72f8
Kris Poedjosoedarmo <neorou@gmail.com>
2017-09-14 08:42:14 -0400

    comments to bootstrap
    make mongodb optional
    add nvm for node js installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,27 +3,34 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install -y python nginx postgresql postgresql-contrib vim git libpq-dev build-essential virtualbox-guest-dkms
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update -y
-sudo apt-get install -y mongodb-org
+# install nvm - as current user
+# if running as root should repeat this as developer/service user
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+bash install_nvm.sh
 
-touch /lib/systemd/system/mongod.service
 
-echo '[Unit]' >> /lib/systemd/system/mongod.service
-echo 'Description=High-performance, schema-free document-oriented database' >> /lib/systemd/system/mongod.service
-echo 'After=network.target' >> /lib/systemd/system/mongod.service
-echo 'Documentation=https://docs.mongodb.org/manual' >> /lib/systemd/system/mongod.service
-echo '' >> /lib/systemd/system/mongod.service
-echo '[Service]' >> /lib/systemd/system/mongod.service
-echo 'User=mongodb' >> /lib/systemd/system/mongod.service
-echo 'Group=mongodb' >> /lib/systemd/system/mongod.service
-echo 'ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf' >> /lib/systemd/system/mongod.service
-echo '' >> /lib/systemd/system/mongod.service
-echo '[Install]' >> /lib/systemd/system/mongod.service
-echo 'WantedBy=multi-user.target' >> /lib/systemd/system/mongod.service
+# uncomment the following to install and start mongodb
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+# echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+# sudo apt-get update -y
+# sudo apt-get install -y mongodb-org
 
-sudo service mongod start
+# touch /lib/systemd/system/mongod.service
+
+# echo '[Unit]' >> /lib/systemd/system/mongod.service
+# echo 'Description=High-performance, schema-free document-oriented database' >> /lib/systemd/system/mongod.service
+# echo 'After=network.target' >> /lib/systemd/system/mongod.service
+# echo 'Documentation=https://docs.mongodb.org/manual' >> /lib/systemd/system/mongod.service
+# echo '' >> /lib/systemd/system/mongod.service
+# echo '[Service]' >> /lib/systemd/system/mongod.service
+# echo 'User=mongodb' >> /lib/systemd/system/mongod.service
+# echo 'Group=mongodb' >> /lib/systemd/system/mongod.service
+# echo 'ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf' >> /lib/systemd/system/mongod.service
+# echo '' >> /lib/systemd/system/mongod.service
+# echo '[Install]' >> /lib/systemd/system/mongod.service
+# echo 'WantedBy=multi-user.target' >> /lib/systemd/system/mongod.service
+
+# sudo service mongod start
 
 if [ ! -d /srv/apps ]; then
   mkdir /srv/apps
